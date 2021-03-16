45cf214fdad1d54d30f4d9276959ff94ff103c78
SoopaKhell <SoopaKhell@users.noreply.github.com>
2021-03-16 00:13:57 -0400

    gohufont

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monaco:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Gohu Gohufont:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
